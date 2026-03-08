0a48122cc0b62d7b3db2177f712f92287ebafcd3
SMT-cmd <timilehintallest@gmail.com>
2026-03-08 16:50:04 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ const CACHE_NAME = 'metaforge-engine-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
+  '/creator/',
+  '/creator/index.html',
+  '/engine/',
+  '/engine/index.html',
   '/manifest.json',
   '/sw.js',
   'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Rajdhani:wght@300;400;500;600;700&display=swap'
